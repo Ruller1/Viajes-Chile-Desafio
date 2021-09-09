9952f075be654fca8104d25271db338db715d2b7
Ruller1 <nicoselyn@gmail.com>
2021-09-08 22:20:08 -0300

    Alert al clickear

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,6 @@
 $(document).ready(function() {
   $('[data-toggle="popover"]').popover();
     $("#botonsito").click(function(){
-        alert("Gracias por enviar tus datos!");
+        alert("Gracias por enviar tus datos!!");
     }); 
 });
